ec2f4d0b43fb128550b04ec0f68afaa898c74838
nokoniko <94224292+nokoniko@users.noreply.github.com>
2025-09-19 15:12:18 +0200

    route alle html sidene og la til audun.html

--- app.py
+++ app.py
@@ -6,5 +6,16 @@ app = Flask(__name__, template_folder='templets')
 def index():
     return render_template('index.html')
 
+@app.route('/audun')
+def audun():
+    return render_template('audun.html')
+
+@app.route('/mio')
+def mio():
+    return render_template('mio.html')
+
+@app.route('/ommeg')
+def ommeg():
+    return render_template('ommeg.html')
 if __name__ == "__main__":
     app.run(debug=True)
