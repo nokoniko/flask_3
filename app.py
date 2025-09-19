945db66d7b2daa8a4f5ff3d6ab4832bccb86c849
nokoniko <94224292+nokoniko@users.noreply.github.com>
2025-09-19 15:05:03 +0200

    La til html filene mine og linka index.html itl flask filen min

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templets')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run
+    app.run(debug=True)
